e7ca0f8f3d278199ef362ba67f1c93142298b47a
jonashogkvist <166859046+jonashogkvist@users.noreply.github.com>
2024-04-23 16:42:41 +0200

    Add gameplay functionality

--- js/main.js
+++ js/main.js
@@ -1,37 +1,80 @@
 function getRandomNumber(num) {
-    return Math.floor(Math.random() * num) + 1;
+  return Math.floor(Math.random() * num) + 1;
 }
-
 
 function getComputerChoice() {
-    let randomChoice = getRandomNumber(3);
-    switch (randomChoice) {
-        case  1 :
-            return "rock";
-        case 2 :
-            return "paper";
-        case 3 : 
-            return "scissors";
-    };
+  let randomChoice = getRandomNumber(3);
+  switch (randomChoice) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    case 3:
+      return "scissors";
+  }
 }
-
 
 function getHumanChoice() {
-    let input = prompt("Choose a move. rock/paper/scissors");
-    if (input === null) {
-        return false
-    } else if (input.toLowerCase() === "rock" || input.toLowerCase() === "paper" || input.toLowerCase() === "scissors") {
-        return input;
-    };
-    return false;
+  let input = prompt("Choose a move. rock/paper/scissors");
+  while (!input) {
+    input = getHumanChoice();
+  }
+  if (
+    input.toLowerCase() === "rock" ||
+    input.toLowerCase() === "paper" ||
+    input.toLowerCase() === "scissors"
+  ) {
+    return input.toLowerCase();
+  }
+  return false;
 }
 
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("tie");
+  } else if (humanChoice === "rock") {
+    if (computerChoice === "scissors") {
+      console.log("Player wins");
+      humanScore += 1;
+    } else {
+      console.log("Computer wins");
+      computerScore += 1;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "rock") {
+      console.log("Player wins");
+      humanScore += 1;
+    } else {
+      console.log("Computer wins");
+      computerScore += 1;
+    }
+  } else if (humanChoice === "scissors") {
+    if (computerChoice === "paper") {
+      console.log("Player wins");
+      humanScore += 1;
+    } else {
+      console.log("Computer wins");
+      computerScore += 1;
+    }
+  }
+}
 
-console.log(getComputerChoice());
-let humanChoice = getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
 
-while (!humanChoice) {
-    humanChoice = getHumanChoice();
-} 
+while (humanScore < 5 && computerScore < 5) {
+  let humanSelection = getHumanChoice();
+  let computerSelection = getComputerChoice();
+  console.log("Computer choice: " + computerSelection);
+  console.log("Human choice: " + humanSelection);
+  playRound(humanSelection, computerSelection);
+  console.log("Current score:");
+  console.log("Computer: " + computerScore);
+  console.log("Human score:" + humanScore);
+}
 
-console.log(humanChoice)
+if (humanScore >= 5) {
+  console.log("Human wins");
+} else {
+  console.log("Computer wins");
+}
